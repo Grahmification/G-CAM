3b19b90f4a35888d03b64775a7cdc3efbe26a157
Graham <kerr.graham.d@gmail.com>
2026-09-12 10:00:33 -0700

    Basic helloworld addin

---
+++ G-CAM/G-CAM/GCamAddin.cs
@@ -0,0 +1,36 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swpublished;
+using System;
+using System.Runtime.InteropServices;
+
+namespace G_CAM
+{
+    public partial class GCamAddin : ISwAddin
+    {
+        private SldWorks _swApp;
+        private int _addinID = -1;
+        private ICommandManager _iCmdMgr;
+
+        public bool ConnectToSW(object ThisSW, int Cookie)
+        {
+            _swApp = ThisSW as SldWorks;
+            _addinID = Cookie;
+
+            //Setup command manager
+            _iCmdMgr = _swApp.GetCommandManager(Cookie);
+
+            _swApp.SendMsgToUser("Loaded Addin");
+            return true;
+        }
+        public bool DisconnectFromSW()
+        {
+            Marshal.ReleaseComObject(_swApp);
+
+            // Call GC.Collect() here in order to retrieve all managed code pointers 
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            return true;
+        }
+    }
+}
---
+++ G-CAM/G-CAM/GCamAddinRegistration.cs
@@ -0,0 +1,98 @@
+﻿using SolidWorks.Interop.swpublished;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace G_CAM
+{
+    /// <summary>
+    /// Portion of the class for registering the addin with SolidWorks
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("df725bf7-4ceb-4425-8931-a072139ddd01")]
+    [DisplayName("G-CAM")]
+    [Description("CAM Addin for SolidWorks")]
+    public partial class GCamAddin : ISwAddin
+    {
+        private const string ADDIN_KEY_TEMPLATE = @"SOFTWARE\SolidWorks\Addins\{{{0}}}";
+        private const string ADDIN_STARTUP_KEY_TEMPLATE = @"Software\SolidWorks\AddInsStartup\{{{0}}}";
+        private const string ADD_IN_TITLE_REG_KEY_NAME = "Title";
+        private const string ADD_IN_DESCRIPTION_REG_KEY_NAME = "Description";
+
+        /// <summary>
+        /// Registers the addin with SolidWorks via the Windows Registry
+        /// </summary>
+        /// <param name="t"></param>
+        [ComRegisterFunction]
+        public static void RegisterFunction(Type t)
+        {
+            try
+            {
+                var addInTitle = "";
+                var loadAtStartup = true;
+                var addInDesc = "";
+
+                var dispNameAtt = t.GetCustomAttributes(false).OfType<DisplayNameAttribute>().FirstOrDefault();
+
+                if (dispNameAtt != null)
+                {
+                    addInTitle = dispNameAtt.DisplayName;
+                }
+                else
+                {
+                    addInTitle = t.ToString();
+                }
+
+                var descAtt = t.GetCustomAttributes(false).OfType<DescriptionAttribute>().FirstOrDefault();
+
+                if (descAtt != null)
+                {
+                    addInDesc = descAtt.Description;
+                }
+                else
+                {
+                    addInDesc = t.ToString();
+                }
+
+                var addInkey = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(
+                    string.Format(ADDIN_KEY_TEMPLATE, t.GUID));
+
+                addInkey.SetValue(null, 0);
+
+                addInkey.SetValue(ADD_IN_TITLE_REG_KEY_NAME, addInTitle);
+                addInkey.SetValue(ADD_IN_DESCRIPTION_REG_KEY_NAME, addInDesc);
+
+                var addInStartupkey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(
+                    string.Format(ADDIN_STARTUP_KEY_TEMPLATE, t.GUID));
+
+                addInStartupkey.SetValue(null, Convert.ToInt32(loadAtStartup), Microsoft.Win32.RegistryValueKind.DWord);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while registering the addin: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Unregisters the addin with SolidWorks via the Windows Registry
+        /// </summary>
+        /// <param name="t"></param>
+        [ComUnregisterFunction]
+        public static void UnregisterFunction(Type t)
+        {
+            try
+            {
+                Microsoft.Win32.Registry.LocalMachine.DeleteSubKey(
+                    string.Format(ADDIN_KEY_TEMPLATE, t.GUID));
+
+                Microsoft.Win32.Registry.CurrentUser.DeleteSubKey(
+                    string.Format(ADDIN_STARTUP_KEY_TEMPLATE, t.GUID));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while unregistering the addin: " + e.Message);
+            }
+        }
+    }
+}
